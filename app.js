b66cfdbdb3d3c976a009cc503a959540ab0abe47
Matt Holloway <mholloway@book.com>
2016-08-28 13:37:02 -0400

    [#1]: Stream data to client

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer();
 server.on('request', function(req,resp) {
-  resp.writeHead(200, {'Content-Type': 'text/html'});
-  resp.end('<h1>Hello</h1><p>How are you</p>');
+  resp.writeHead(200, {'Content-Type': 'text/json'});
+  fs.createReadStream('./resource.json').pipe(resp);
 });
 server.listen( 8080, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8080/');
