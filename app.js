d418481fec05cabae2a0d67516337cae629d826b
Matt Holloway <mholloway@book.com>
2016-08-29 15:42:43 -0400

    [#2]: setup for CH2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var server = http.createServer();
 server.on('request', function(req,resp) {
   resp.writeHead(200, {'Content-Type': 'text/json'});
   fs.createReadStream('./resource.json').pipe(resp);
+  console.log(resp.statusCode);
 });
 server.listen( 8080, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8080/');
