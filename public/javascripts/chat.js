959051844761dee2f4b249f77699dd37df2962f3
Matt Holloway <mholloway@book.com>
2016-09-02 21:29:20 -0400

    [#2]: some bug in here now, messages and names not properly broadcast, will fix

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -16,9 +16,9 @@ Chat.prototype.changeRoom = function(room) {
   });
 };
 
-Chat.prototype.proccessCommand = function(command) {
+Chat.prototype.processCommand = function(command) {
   var words = command.split(' ');
-  var command = words[0].substring(1, words[0].length).toLowercase();
+  var command = words[0].substring(1, words[0].length).toLowerCase();
   var message = false;
   switch (command) {
     case 'join':
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,3 +1,5 @@
+var socket = io.connect();
+
 function divEscapedContentElement(message) {
   return $('<div></div>').text(message);
 }
@@ -22,3 +24,50 @@ function processUserInput(chatApp, socket) {
   }
   $('#send-message').val('');
 }
+
+//execute on document load
+$(document).ready(function() {
+  var chatApp = new Chat(socket);
+
+  socket.on('nameResult', function(result) {
+    var message;
+
+    if (result.success) {
+      message = 'You are now known as ' + result.name + '.';
+    } else {
+      message = result.message;
+    }
+    $('#message').append(divSystemContentElement(message));
+  });
+
+  socket.on('joinResult', function(result) {
+    $('#room').text(result.room);
+    $('#messages').append(divSystemContentElement('Room changed.'));
+  });
+
+  socket.on('rooms', function(rooms) {
+    $('#room-list').empty();
+    for(var room in rooms) {
+      room = room.substring(1, room.length);
+      if (room != '') {
+        $('#room-list').append(divEscapedContentElement(room));
+      }
+    }
+
+    $('#room-list div').click(function() {
+      chatApp.processCommand('/join ' + $(this).text());
+      $('#send-message').focus();
+    });
+  });
+
+  setInterval(function() {
+    socket.emit('rooms');
+  },1000);
+
+  $('#send-message').focus();
+
+  $('#send-form').submit(function() {
+    processUserInput(chatApp, socket);
+    return false;
+  });
+});
