47fc9e5db242734c80cfac18c19b131616b95ca9
Matt Holloway <mholloway@book.com>
2016-09-11 20:52:35 -0400

    [#3]: refine export to module.export, refine with prototypes to return a Currency constructor

--- test-currency.js
+++ test-currency.js
@@ -1,4 +1,6 @@
-var currency = require('./currency');
+var Currency = require('./currency');
+var canadianDollar = 0.91;
+var currency = new Currency(canadianDollar);
 
 console.log('50 Canadian dollars equals $' + currency.canadianToUS(50) + ' USD');
 console.log('50 US dollars equals $' + currency.USToCanadian(50) + ' CAD');
